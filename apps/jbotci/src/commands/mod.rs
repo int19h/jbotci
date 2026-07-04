c987aa5183e103b34e631db299d2d873ea65e561
Pavel Minaev <me@int19h.org>
2026-07-04 07:55:38 -0700

    Ref #212: move vlacku command into module

--- apps/jbotci/src/commands/mod.rs
+++ apps/jbotci/src/commands/mod.rs
@@ -1,3 +1,7 @@
 mod setup;
+mod vlacku;
 
 pub(super) use setup::run_setup;
+pub(super) use vlacku::{VlackuRenderOptions, render_vlacku_output_with_options, run_vlacku};
+#[cfg(test)]
+pub(super) use vlacku::{render_vlacku_output, render_vlacku_output_with_width};
